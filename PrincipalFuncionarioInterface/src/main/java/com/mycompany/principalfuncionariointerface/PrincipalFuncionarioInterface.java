dba3259dc637474a81564ad705ec7307d1854377
Ruan55 <114105714+Ruan55@users.noreply.github.com>
2024-08-28 15:01:36 -0300

    Update PrincipalFuncionarioInterface.java

--- PrincipalFuncionarioInterface/src/main/java/com/mycompany/principalfuncionariointerface/PrincipalFuncionarioInterface.java
+++ PrincipalFuncionarioInterface/src/main/java/com/mycompany/principalfuncionariointerface/PrincipalFuncionarioInterface.java
@@ -11,6 +11,10 @@ package com.mycompany.principalfuncionariointerface;
 public class PrincipalFuncionarioInterface {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+    Motoboy motoboy1 = new Motoboy("Adsa2312", "Ruan", "04/08/2002", Sexo.MASCULINO, Setor.OPERACOES, 3233);
+    Diretor diretor1 = new Diretor("Marcos", "21/05/1991", Sexo.MASCULINO, Setor.RECURSOS_HUMANOS, 4566);
+
+        System.out.println(motoboy1.getSalarioFinal());
+        System.out.println(diretor1.getSalarioFinal());
     }
 }

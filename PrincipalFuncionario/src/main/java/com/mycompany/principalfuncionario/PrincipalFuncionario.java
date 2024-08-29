6de4032a8f931db0586b270f54b972a5f3f7e9f5
Ruan55 <114105714+Ruan55@users.noreply.github.com>
2024-08-29 10:46:17 -0300

    Update PrincipalFuncionario.java

--- PrincipalFuncionario/src/main/java/com/mycompany/principalfuncionario/PrincipalFuncionario.java
+++ PrincipalFuncionario/src/main/java/com/mycompany/principalfuncionario/PrincipalFuncionario.java
@@ -12,7 +12,7 @@ public class PrincipalFuncionario {
 
     public static void main(String[] args) {
     Advogado advogado1 = new Advogado("32132", "Paula", "3232233", "321321",
-    new Endereco("Rua C", "32", "N/A", "323213", "Salvador", UnidadeFederativa.BAHIA), Setor.MARKETING, Sexo.FEMININO, 3213, "03/04/1999");
+    new Endereco("Rua C", "32", "N/A", "323213", "Salvador", UnidadeFederativa.BAHIA), Setor.JURIDICO, Sexo.FEMININO, 3213, "03/04/1999");
     Motorista motorista1 = new Motorista("asdf3237", "Tiago", "3213321", "31233",
     new Endereco("Rua I", "76", "N/A", "213123", "São Paulo", UnidadeFederativa.SAO_PAULO), Setor.OPERACOES, Sexo.MASCULINO, 1567, "17/08/1978");
     Gerente gerente1 = new Gerente(Bonificacao.GERENTE, "Jonatas", "323223", "3332121",
